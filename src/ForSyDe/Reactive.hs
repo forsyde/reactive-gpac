56d9f3714fc4ca4673d85e319c1b091994282f3a
José Edil Guimarães De Medeiros <jose.edil@gmail.com>
2017-07-19 15:49:11 +0200

    Add cabal project files.

---
+++ src/ForSyDe/Reactive.hs
@@ -0,0 +1,17 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  ForSyDe.Reactive
+-- Copyright   :  (c) José Edil Guimarães de Medeiros
+-- License     :
+--
+-- Maintainer  :  j.edil@ene.unb.br
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-- Arrow and FRP inspired implementation for ForSyDe.
+--
+-----------------------------------------------------------------------------
+
+module ForSyDe.Reactive () where
+
+import ForSyDe.Reactive.Synchronous
