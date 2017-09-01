639a1c7af10d71fddfad9745323f986b2bd4592b
Edil Medeiros <j.edil@ene.unb.br>
2017-09-01 16:34:10 +0200

    New demos

--- demos/CT/demoCT1.hs
+++ demos/CT/demoCT1.hs
@@ -30,18 +30,51 @@ s2 = fst $ execCT p2 t1
 s3 = fst $ execCT p3 t1
 
 
--- | Uniform sampling
+-- | Nonuniform sampling
 t2 = chebyspace 0 (5*2*pi) 50
 s4 = fst $ execCT p4 t2
 s5 = fst $ execCT p5 t2
 
--- | Chebyshev (nonuniform) sampling
+-- | Chebyshev (nonuniform) sampling. This has common points compared
+-- to t4 and the system is expected to yield the same results on
+-- those.
 t3 = chebyspace 0 (5*2*pi) 99
 s4' = fst $ execCT p4 t3
 s5' = fst $ execCT p5 t3
 
+-- | "Efficiency" verification: put a big number on n
+n = 1000
+t4 = linspace 0 (5*2*pi) n
+s4'' = fst $ execCT p4 t4
+s5'' = fst $ execCT p5 t4
+
+
+-- | Testing "discrete" changes...
+pp1 = time >>>* liftCT change
+  where change t
+          | t <= (t2 !! 25) = 1.0
+          | otherwise = (-1.0)
+
+pp2 = liftCT (uncurry (-))
+
+pp3 = feedCT (liftCT id) >>>* firstCT pp1 >>>* pp2
+
+tt = linspace 0 10 100
+ss = fst $ execCT (intCT rk4' 0 0 pp3) tt
+
+ss1 = fst $ execCT (intCT rk4' 0 0 pp3) t2
+ss2 = fst $ execCT (intCT rk4' 0 0 pp3) t3
+
+
 main = do
-  writeDump "demos/plot/unif_sin.dat" s4
-  writeDump "demos/plot/unif_int_sin.dat" s5
-  writeDump "demos/plot/cheby_sin.dat" s4'
-  writeDump "demos/plot/cheby_int_sin.dat" s5'
+  writeDump "demos/plot/cheby_sin1.dat" s4
+  writeDump "demos/plot/cheby_int_sin1.dat" s5
+  writeDump "demos/plot/cheby_sin2.dat" s4'
+  writeDump "demos/plot/cheby_int_sin2.dat" s5'
+
+  writeDump "demos/plot/eff_sin.dat" s4''
+  writeDump "demos/plot/eff_int_sin.dat" s5''
+
+  writeDump "demos/plot/rc_filter.dat" ss
+  writeDump "demos/plot/cheby_rc1.dat" ss1
+  writeDump "demos/plot/cheby_rc2.dat" ss2
