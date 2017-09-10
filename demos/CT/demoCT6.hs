480a616bcc572880822ee1052ae463e15d11d46b
José Edil Guimarães De Medeiros <jose.edil@gmail.com>
2017-09-10 22:06:21 +0200

    Found the bug. How to solve?

--- demos/CT/demoCT6.hs
+++ demos/CT/demoCT6.hs
@@ -2,6 +2,8 @@ module DemoCT6 where
 
 import ForSyDe.Reactive
 
+-- | Found the bug: constCT (-1) on p1 definition ignores the time input. There is an implicit time input which is accessible via the `at` function and so far I can not alter time from that...
+
 -- | Sine circuit
 sineCirc = intCT rk4 0 0 p1
   where p1 = (constCT (-1) *** idCT) >>> multCT >>> openLoopInt
@@ -17,8 +19,15 @@ s1 = time >>> sineCirc
 s2 = time >>> tShift (pi/2) >>> sineCirc
   where tShift k = (idCT &&& constCT k) >>> adderCT
 
+s2' = time >>> tShift (pi/2)
+  where tShift k = (idCT &&& constCT k) >>> adderCT
+
+
 -- | Time scaled process
 s3 = time >>> tScale 2 >>> sineCirc
+  where tScale k = (idCT &&& constCT k) >>> multCT
+
+s3' = time >>> tScale 2
   where tScale k = (idCT &&& constCT k) >>> multCT
 
 -- | Observation points
